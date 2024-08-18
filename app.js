7c23cf1a16713073351eb85b1daa4b1ca870b3b0
pradeep8577 <pradeeppawar8577@gmail.com>
2024-08-19 00:15:36 +0530

    env update

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 // create simple server which retunr hello 
 
 const express = require('express')
-
 const app = express()
+const dotenv = require("dotenv");
+dotenv.config();
 
 
-
+const PORT = process.env.PORT;
 
 
 app.get('/', (req, res) => {
     res.send('Hello')
 })
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000')
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
 })
