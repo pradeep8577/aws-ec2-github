19ac74ba8bb2f0a85d2bff2dafa73f4124ef964d
pradeep8577 <pradeeppawar8577@gmail.com>
2024-08-19 00:13:10 +0530

    cohere: init

---
+++ app.js
@@ -0,0 +1,17 @@
+// create simple server which retunr hello 
+
+const express = require('express')
+
+const app = express()
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello')
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000')
+})
